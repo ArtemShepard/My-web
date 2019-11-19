01f8f9c014cd5772bda9f12ec7e893e378b27cf2
Jarry_shepard <artem.kozar@gmail.com>
2019-11-20 02:56:13 +0500

    RANDOM SPAWN

--- javascript/javascript6.js
+++ javascript/javascript6.js
@@ -3,9 +3,8 @@
 // рандомный размер
 // рандомное расположение
 var randomfigure;
-var randomcolor;
 var width;
-var location 
+var min;
 
 
 
@@ -15,7 +14,20 @@ if(Math.floor(Math.random()*2) == 1){
 else{
 	randomfigure = "circle";
 }
-console.log(randomfigure);
 document.getElementById(randomfigure).style.display = "block";
 
 document.getElementById(randomfigure).style.backgroundColor = "#"+Math.floor(Math.random()*999999);
+	widthGf = Math.floor(Math.random()*250);
+document.getElementById(randomfigure).style.height = widthGf + "px";
+document.getElementById(randomfigure).style.width = widthGf +"px";
+document.getElementById(randomfigure).style.top = Math.floor(Math.random()*window.screen.height) +"px";
+document.getElementById(randomfigure).style.left = Math.floor(Math.random()*window.screen.width) +"px";
+
+while(document.getElementById(randomfigure).style.top < widthGf/2 || document.getElementById(randomfigure).style.top > window.screen.height-widthGf){
+	document.getElementById(randomfigure).style.top = Math.floor(Math.random()*window.screen.height) +"px";
+}
+while(document.getElementById(randomfigure).style.left < widthGf/2 || document.getElementById(randomfigure).style.left > window.screen.width-widthGf){
+	document.getElementById(randomfigure).style.left = Math.floor(Math.random()*window.screen.width) +"px";
+}
+	
+
