42d12121ecb25ca553e736ed6bfe2d80a9049149
ArtemShepard <artem.kozar@gmail.com>
2019-11-20 15:11:51 +0500

    spawn bug

--- javascript/javascript6.js
+++ javascript/javascript6.js
@@ -18,16 +18,22 @@ document.getElementById(randomfigure).style.display = "block";
 
 document.getElementById(randomfigure).style.backgroundColor = "#"+Math.floor(Math.random()*999999);
 	widthGf = Math.floor(Math.random()*250);
+	console.log(widthGf + " размер фигуры");
 document.getElementById(randomfigure).style.height = widthGf + "px";
 document.getElementById(randomfigure).style.width = widthGf +"px";
 document.getElementById(randomfigure).style.top = Math.floor(Math.random()*window.screen.height) +"px";
+	console.log(window.screen.height + " высота экрана");
+	console.log(document.getElementById(randomfigure).style.top + " высота спавна");
 document.getElementById(randomfigure).style.left = Math.floor(Math.random()*window.screen.width) +"px";
-
+	console.log(window.screen.width + " ширина экрана");
+	console.log(document.getElementById(randomfigure).style.left + " ширина спавна");
 while(document.getElementById(randomfigure).style.top < widthGf/2 || document.getElementById(randomfigure).style.top > window.screen.height-widthGf){
 	document.getElementById(randomfigure).style.top = Math.floor(Math.random()*window.screen.height) +"px";
 }
+console.log(window.screen.height-widthGf + " высота экрана - размер фигуры")
 while(document.getElementById(randomfigure).style.left < widthGf/2 || document.getElementById(randomfigure).style.left > window.screen.width-widthGf){
 	document.getElementById(randomfigure).style.left = Math.floor(Math.random()*window.screen.width) +"px";
 }
+console.log(window.screen.width-widthGf + " ширина экрана - размер фигуры")
 	
 
